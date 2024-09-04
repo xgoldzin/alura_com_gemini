374d439808768b0422337d9c7ce31af620e49876
xgoldzin <gustafera34@gmail.com>
2024-09-04 19:36:24 -0300

    second commit

---
+++ js/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo : "Controlador Lógico Programável",
+        descricao : "Descrição: O cérebro da automação. Um dispositivo eletrônico que pode ser programado para executar uma série de tarefas lógicas, como controlar motores, sensores e atuadores. CLPs são essenciais para automatizar processos industriais e tomar decisões com base em informações recebidas.",
+        link : "https://blog.cetro.com.br/2021/04/13/o-que-e-um-controlador-logico-programavel/?gad_source=1&gclid=Cj0KCQjwiuC2BhDSARIsALOVfBIHx_gu09sUcZ-B4TelmRwwfN0oTl1kZrMvHmj1Wi9KBcPMhM2vsBgaAo7NEALw_wcB"
+    },
+    {
+        titulo : "Servo Motor",
+        descricao : "Descrição: Motor elétrico de alta precisão, utilizado para controlar a posição,velocidade e torque de um eixo. Servo motores são amplamente utilizados em robótica, máquinas CNC e outros equipamentos que exigem movimentos precisos e repetitivos.",
+        link : "https://www.galil.com/news/motion-controllers/servo-motor-controller-basics?gad_source=1&gclid=Cj0KCQjwiuC2BhDSARIsALOVfBJCHYD_YT6MorA81Cwfkh-rW8UJiF1yZThi7DepEsLzWyJ6OdBEOW0aAunuEALw_wcB"
+    },
+    {
+        titulo : "Sensor",
+        descricao : "Descrição: Dispositivo que detecta mudanças em uma variável física, como temperatura,pressão,posição ou fluxo. Os sensores fornecem dados em tempo real para o CLP, permitindo que osistema tome decisões e ajuste os processos de acordo com as condições.",
+        link : "https://www.mundodaeletrica.com.br/o-que-sao-sensores-e-quais-as-suas-aplicacoes/"
+    }
+]
